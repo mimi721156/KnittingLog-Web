2c6a47666c2e7dfeff331c3dbae976f8ac6fe9d9
mimi721156 <63344148+mimi721156@users.noreply.github.com>
2026-01-06 13:44:27 +0800

    Update storage.js

--- src/storage.js
+++ src/storage.js
@@ -1,1 +1,17 @@
 
+import { getJsonViaContentsApi, putJsonViaContentsApi } from "./githubContentsApi.js";
+
+export async function loadData(cfg) {
+  const { json, sha } = await getJsonViaContentsApi(cfg);
+  return { data: json, sha };
+}
+
+export async function saveData(cfg, data, sha) {
+  data.updatedAt = new Date().toISOString();
+  return await putJsonViaContentsApi({
+    ...cfg,
+    jsonObject: data,
+    sha,
+    message: `Update knitting log (${new Date().toISOString()})`,
+  });
+}
